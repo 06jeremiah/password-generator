3919c4e24e13178e2f71b258510e4a6b524c7b54
Jeremiah Weatherspoon <06jeremiah@gmail.com>
2020-07-07 23:01:47 -0400

    added more js and functions

--- script.js
+++ script.js
@@ -1,90 +1,100 @@
+var generateBtn = document.querySelector("#generate");
 
-//GIVEN I need a new, secure password
+// Arrays of characters for the password
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var num = "0123456789";
+var specialChar = "!@#$%^&*()_+";
 
-//WHEN I click the button to generate a password
 
-//THEN I am presented with a series of prompts for password criteria
-
-//WHEN prompted for password criteria
-
-//THEN I select which criteria to include in the password
-
-//Assignment Code
-var generateBtn = document.querySelector("#generateBtn");
-var createPasswordBtn = document.querySelector("#createPassBtn");
-var new_password_display = document.querySelector("#password");
-var length_num = document.querySelector("#passwordLength");
-var isLowerCaseAllowed = document.querySelector("#lowerCase");
-var isUpperCaseAllowed = document.querySelector("#upperCase");
-var isNumbersAllowed = document.querySelector("#numbersAllowed");
-var isSpecialCharAllowed = document.querySelector("#specialChar");
-
-var popUpBox = document.querySelector("#optionDialog");
-
-// possible characters
-var length = 8;
-var letterLowerCase = "abcdefghijklmnopqrstuvwxyz";
-var letterUpperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var specialChar = "!@#$%^&*()-";
-
-console.log(a,b,c,d,e,f,g,h,i,j,)
-
-//WHEN prompted for character types to include in the password
-generateBtn.onclick = promptPasswordOptions;
-createPasswordBtn.onclick = CreatePassword;
-
-function promptPasswordOptions(){
-    $('#optionDialog').dialog({
-        height: 450,
-        width:450,        
-        title: 'Password Options',
-        classes:'dialogStyle',
-        modal:true,
-        position:'center',
-        open:function(){
-            length_num.value = 8;
-        }
-       
-       
-    });
-}
-
-//THEN I choose lowercase, uppercase, numeric, and/or special characters
-
-//WHEN I answer each prompt
+//this function generates the password
+function generatePassword() {
+  var avaiableChars = ""
+  var guaranteedChars = ""
  
+  
+  var numberOfCharacters = prompt("How many characters do you want the password to be between 10 to 50?")
+  
+  // used a while loop: length of password at least 10 characters and no more than 100 characters
+  while (numberOfCharacters <= 9 || numberOfCharacters === 50) {
+    
+    // if specified condition is false it will prompt the alert
+    alert("Password length must be between 10 to 50 characters, try again please");
+    //2nd prompt will come up after the alert
+    numberOfCharacters = prompt("How many characters do you want the password to be between 10 to 50?")
+    
+    // message the console with number of characters selected by the user
+    console.log("len: " + numberOfCharacters)
+  }
+  
+  //3rd prompt for the user if above statement is true
+  var lowerCase = confirm("do you want lowercase characters for your password?");
+  
+  //message the console with the character selected by the user
+  console.log('lower Case: ' + lowerCase);
+  
+  //4th prompt for the user
+  var upperCase = confirm("do you want upper characters for your password?");
+  
+  //message the console with the character selected by the user
+  console.log('Upper Case: ' + upperCase);
+  
+  //5th prompt for the user
+  var numeric = confirm("do you want numbers for your password?");
+  
+  //message the console with the character selected by the user
+  console.log('Numeric ' + numeric);
+  
+  //6th prompt for the user
+  var punctuation = confirm("do you want special characters for your password?");
+  
+  //message the console with the character selected by the user
+  console.log('Punctuation ' + punctuation);
 
+  //if lowercase variable is true it will random pick a letter from lowerChars 
+  if (lowercase) {
+    guaranteedChars += randomize(lowerChars)
+    avaiableChars += lowerChars
+  }
+  //if uppercase variable is true it will random pick a letter from upperChars
+  if (upperCase) {
+    guaranteedChars += randomize(upperChars)
+    avaiableChars += upperChars
+  }
+  //if numeric variable is true it will random pick a letter from munberChars
+  if (numeric) {
+    guaranteedChars += randomize(numberChars)
+    avaiableChars += numberChars
 
+  }
+  //if punctuation variable is true it will random pick a letter from punctuationChars
+  if (punctuation) {
+    guaranteedChars += randomize(punctuationChars)
+    avaiableChars += punctuationChars
 
-//THEN my input should be validated and at least one character type should be selected
-function validateSelection(){
-
-    if(isLowerCaseAllowed.checked == false && isUpperCaseAllowed.checked == false 
-        && isNumbersAllowed.checked == false && isSpecialCharAllowed.checked == false){
-        return false;
-    }        
-    return true;
+  }
+  //used the for loop: the var i = guaranteedChars by the length of the string, i must be less than number of "numberOfCharacters", i increases the value
+  var password = guaranteedChars;
+  for (let i = guaranteedChars.length; i < numberOfCharacters; i++) {
+    //adding password string to randomize function
+    password += randomize(avaiableChars)
+  }
+  //The return statement stops the execution of function and returns a value from that function
+  return password
+  
 }
-function CreatePassword(){
-    if(validateSelection()== false){
-        alert("");
-        return false;
-    }
-
-//generate code goes here!!
-    var randomPassword = k;
-    new_password_display.value =new_pass;
-
-
-
-
+//function picks one random character from string
+function randomize(characters) {
+  //math.floor Math.random returns random characters + the length to the user 
+  var randomIndex = Math.floor(Math.random() * characters.length);
+  //the return statement stops the execution of function and returns a value from that function
+  return characters[randomIndex]
 }
-
-//THEN a password is generated that matches the selected criteria
-
-//WHEN the password is generated
-
-//THEN the password is either displayed in an alert or written to the page
-
-
+//this function calls another function is hooking into 2 elements for the password
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+//when user clicked button it writes the function above
+generateBtn.addEventListener("click", writePassword);
